f572073d8a58e0f3649670e45ef5ec050ef7f973
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2023-06-10 14:41:24 +0100

    FrameID Setter fix

--- io_scene_foundry/tools/set_frame_ids.py
+++ io_scene_foundry/tools/set_frame_ids.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2020 Generalkidd & Crisp
+# Copyright (c) 2023 Generalkidd & Crisp
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,14 +25,11 @@
 # ##### END MIT LICENSE BLOCK #####
 
 import os
-import ctypes
 import xml.etree.ElementTree as ET
-from subprocess import Popen
 
 from io_scene_foundry.utils.nwo_utils import (
     get_tags_path,
     get_ek_path,
-    get_tool_path,
     frame_prefixes,
     get_prefix,
     run_tool,
@@ -40,9 +37,8 @@ from io_scene_foundry.utils.nwo_utils import (
     comma_partition,
 )
 
-
 def reset_frame_ids(context, report):
-    model_armature = GetArmature(context, report)
+    model_armature = get_armature(context, report)
     if model_armature != None:
         blend_bones = model_armature.data.bones
         for b in blend_bones:
@@ -55,20 +51,20 @@ def reset_frame_ids(context, report):
 
 
 def set_frame_ids(context, report):
-    model_armature = GetArmature(context, report)
+    model_armature = get_armature(context, report)
     frame_count = 0
     if model_armature != None:
         try:
-            framelist = ImportTagXML(context, report)
+            framelist = import_tag_xml(context, report)
             if not framelist == None:
-                tag_bone_names = CleanBoneNames(framelist)
-                blend_bone_names = CleanBones(model_armature.data.bones)
+                tag_bone_names = clean_bone_names(framelist)
+                blend_bone_names = clean_bones(model_armature.data.bones)
                 blend_bones = model_armature.data.bones
 
                 for blend_bone in blend_bone_names:
                     for tag_bone in tag_bone_names:
                         if blend_bone == tag_bone:
-                            ApplyFrameIDs(blend_bone, blend_bones, framelist)
+                            apply_frame_ids(blend_bone, blend_bones, framelist)
                             frame_count += 1
 
                 report(
@@ -81,23 +77,23 @@ def set_frame_ids(context, report):
     return {"FINISHED"}
 
 
-def ApplyFrameIDs(bone_name, bone_names_list, framelist):
+def apply_frame_ids(bone_name, bone_names_list, framelist):
     for b in bone_names_list:
-        if CleanBone(b) == bone_name:
-            b.nwo.frame_id1 = GetID(1, bone_name, framelist)
-            b.nwo.frame_id2 = GetID(2, bone_name, framelist)
+        if clean_bone(b) == bone_name:
+            b.nwo.frame_id1 = get_id(1, bone_name, framelist)
+            b.nwo.frame_id2 = get_id(2, bone_name, framelist)
 
 
-def GetID(ID, name, framelist):
+def get_id(ID, name, framelist):
     frame = ""
     for x in framelist:
-        b_name = CleanBoneName(x[0])
+        b_name = clean_bone_name(x[0])
         if b_name == name:
             frame = x[ID]
     return frame
 
 
-def CleanBoneNames(bones):
+def clean_bone_names(bones):
     cleanlist = []
     for b in bones:
         prefix = get_prefix(b[0], frame_prefixes)
@@ -107,7 +103,7 @@ def CleanBoneNames(bones):
     return cleanlist
 
 
-def CleanBones(bones):
+def clean_bones(bones):
     cleanlist = []
     for b in bones:
         prefix = get_prefix(b.name, frame_prefixes)
@@ -117,24 +113,24 @@ def CleanBones(bones):
     return cleanlist
 
 
-def CleanBone(bone):
+def clean_bone(bone):
     prefix = get_prefix(bone.name, frame_prefixes)
     cleaned_bone = bone.name.removeprefix(prefix)
 
     return cleaned_bone
 
 
-def CleanBoneName(bone):
+def clean_bone_name(bone):
     prefix = get_prefix(bone, frame_prefixes)
     cleaned_bone = bone.removeprefix(prefix)
 
     return cleaned_bone
 
 
-def ImportTagXML(context, report):
+def import_tag_xml(context, report):
     # try:
     xml_path = get_tags_path() + "temp.xml"
-    tag_path = GetGraphPath(context)
+    tag_path = get_graph_path(context)
     print(os.path.join(get_ek_path(), tag_path))
     if not os.path.exists(os.path.join(get_ek_path(), tag_path)):
         report(
@@ -150,7 +146,7 @@ def ImportTagXML(context, report):
             "Failed to convert supplied tag path to XML. Did you enter a valid tag path?",
         )
         return None
-    bonelist = ParseXML(xml_path, context)
+    bonelist = parse_xml(xml_path, context)
     os.remove(xml_path)
     return bonelist
     # except:
@@ -158,7 +154,7 @@ def ImportTagXML(context, report):
     #     return None
 
 
-def GetGraphPath(context):
+def get_graph_path(context):
     path = context.scene.nwo_frame_ids.anim_tag_path
     # path cleaning
     path = path.strip("\\")
@@ -171,10 +167,10 @@ def GetGraphPath(context):
     return path
 
 
-def ParseXML(xmlPath, context):
+def parse_xml(xmlPath, context):
     parent = []
     scene = context.scene
-    scene_nwo = scene.nwo_global
+    scene_nwo = scene.nwo
 
     if scene_nwo.game_version in ("h4", "h2a"):
         tree = ET.parse(xmlPath, parser=ET.XMLParser(encoding="iso-8859-5"))
@@ -215,12 +211,10 @@ def ParseXML(xmlPath, context):
     return parent
 
 
-def GetArmature(context, report):
+def get_armature(context, report):
     model_armature = None
-    for ob in context.scene.objects:
-        if ob.type == "ARMATURE" and not ob.name.startswith(
-            "+"
-        ):  # added a check for a '+' prefix in armature name, to support special animation control armatures in the future
+    for ob in context.view_layer.objects:
+        if ob.type == "ARMATURE":
             model_armature = ob
             break
     if model_armature == None:
