913e327dcde2212d4e584c30b66c1d3dae08f919
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2024-10-20 02:00:11 +0100

    ik matrix local

--- blender/addons/io_scene_foundry/export/process.py
+++ blender/addons/io_scene_foundry/export/process.py
@@ -1301,7 +1301,7 @@ class ExportScene:
                             proxy_target.parent_type = 'BONE'
                             proxy_target.parent_bone = event.ik_target_marker.parent_bone
                             
-                        proxy_target.matrix_world = event.ik_target_marker.matrix_world
+                        proxy_target.matrix_local = event.ik_target_marker.matrix_local
                         
                     event_ob_props[proxy_target] = proxy_target_props
                     
@@ -1320,7 +1320,7 @@ class ExportScene:
                     effector_props["bungie_animation_control_ik_effect"] = event.ik_influence
                     effector.parent_type = proxy_target.parent_type
                     effector.parent_bone = proxy_target.parent_bone
-                    effector.matrix_world = proxy_target.matrix_world
+                    effector.matrix_local = proxy_target.matrix_local
                     
                     event_ob_props[effector] = effector_props
                     
