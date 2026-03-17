b65e20d38a008fa96d03bbfec87321152f8fe0ef
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2026-03-17 22:58:18 +0000

    temp remove node arrange

--- blender/addons/io_scene_foundry/tools/node_tree_arrange/__init__.py
+++ blender/addons/io_scene_foundry/tools/node_tree_arrange/__init__.py
@@ -4,6 +4,7 @@ from .sugiyama import sugiyama_layout
 from . import config
 
 def arrange(tree: bpy.types.NodeTree):
+    return # Until I can figure out why this crashes
     config.selected = list(tree.nodes)
     config.MARGIN = Vector((100, 100)).freeze()
     config.tree = tree
